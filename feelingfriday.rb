c6b0faf5d2d4502fa0aad7578f82b7e179cd39c6
Anthony Wijnen <anthony.wijnen@gmail.com>
2013-03-19 10:09:50 -0400

    re add get complete

--- feelingfriday.rb
+++ feelingfriday.rb
@@ -1,7 +1,11 @@
+# Include the necesarry gems #
+
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 require 'dm-postgres-adapter'
+
+# Set up the Database #
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
@@ -32,12 +36,17 @@ end
 
 DataMapper.finalize.auto_upgrade! # automatically update the database to contain the tables and fields we have set, and to do so again if we make any changes to the schema
 
+
+# Routes #
+
+# Get all notes from the database for display
 get '/' do
   @notes = Note.all :order => :id.desc
   @title = "All Emotions"
   erb :home
 end
 
+# Create a new note
 post '/post' do
   n = Note.new
   n.content = params[:content]
@@ -47,17 +56,14 @@ post '/post' do
   redirect '/'
 end
 
-# get '/migrate' do
-#   DataMapper.auto_migrate!
-#   redirect '/'
-# end
-
+# Display an individual note using it's 'id', and present the user with an edit view.
 get '/:id' do  
   @note = Note.get params[:id]  
   @title = "Edit note ##{params[:id]}"  
   erb :edit  
 end 
 
+# Edit an individual note, and update the database using the HTTP verb 'put'
 put '/:id' do  
   n = Note.get params[:id]  
   n.content = params[:content]  
@@ -67,18 +73,21 @@ put '/:id' do
   redirect '/'  
 end
 
+# Display an individual note using it's 'id', and present the user with a delete view.
 get '/:id/delete' do  
   @note = Note.get params[:id]  
   @title = "Confirm deletion of note ##{params[:id]}"  
   erb :delete  
 end
 
+# Delete an individual note, and remove it from the database using the HTTP verb 'delete'
 delete '/:id' do  
   n = Note.get params[:id]  
   n.destroy  
   redirect '/'  
 end 
 
+# flip complete status
 get '/:id/complete' do  
   n = Note.get params[:id]  
   n.complete = n.complete ? 0 : 1 # flip it  
@@ -87,6 +96,7 @@ get '/:id/complete' do
   redirect '/'  
 end
 
+# Post a comment to a note
 post '/comment' do
   p params['id'], params['content']
   note = Note.get(params['id'])
